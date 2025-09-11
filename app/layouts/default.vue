217b5cd9e7074b13680b6323da104097cb02a9bd
Daniel <usrrufus12@omint.com.br>
2025-09-10 21:55:41 -0300

    implementada navbar

---
+++ app/layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <UNavigationMenu collapsed :items="items" class="p-4 shadow-lg" color="success" />
+    <UContainer class="min-h-screen">
+      <slot />
+    </UContainer>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui';
+
+const items = ref<NavigationMenuItem[]>([
+  {
+    label: 'Triagem de currículos',
+    icon: 'mdi-file-search',
+    to: '/triagem',
+  }
+])
+</script>
